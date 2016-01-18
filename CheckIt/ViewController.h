deb8cfb12009d761009889d07b4d3cde75ba393f
DmitryKretsu <dmitry.kretsu@gmail.com>
2016-01-18 15:22:13 +0300

    initial commit

---
+++ CheckIt/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CheckIt
+//
+//  Created by DmitryKretsu on 1/18/16.
+//  Copyright © 2016 Weezlabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CheckIt/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CheckIt
+//
+//  Created by DmitryKretsu on 1/18/16.
+//  Copyright © 2016 Weezlabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
