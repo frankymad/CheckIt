fcb7717ce770a88b76ef06f29ff0737d004022a0
DmitryKretsu <dmitry.kretsu@gmail.com>
2016-01-22 18:31:53 +0300

    Add function "Delete task" and group editing. Delete .idea folder from the project.

--- CheckIt/Classes/ViewControllers/CITaskTableViewController.m
+++ CheckIt/Classes/ViewControllers/CITaskTableViewController.m
@@ -19,20 +19,36 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    self.tasks = @[@{@"task" : @"Work hard!", @"description" : @"You must work harded)"},
-                   @{@"task" : @"Learn delegates!", @"description" : @"You need them)"},
-                   @{@"task" : @"Learn protocols!", @"description" : @"You need them to!)"
-                     }].mutableCopy;
+    self.tasks = @[@{@"task" : @"Work hard!", @"description" : @"You must work harded)", @"category" : @"Home"},
+                   @{@"task" : @"Learn delegates!", @"description" : @"You need them)", @"category" : @"Work"},
+                   @{@"task" : @"Learn protocols!", @"description" : @"You need them to!)", @"category" : @"Home"}
+                   ].mutableCopy;
     self.navigationItem.title = @"Check it";
+    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Edit" style:UIBarButtonItemStylePlain target:self action:@selector(editing:)];
     self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(addNewTask:)];
 }
 
+#pragma mark - editing
 
+-(void)editing:(UIBarButtonItem *)sender {
+    [self.tableView setEditing:!self.tableView.editing animated:YES];
+    
+    if (self.tableView.editing) {
+        sender.title = @"Done";
+        sender.style = UIBarButtonItemStyleDone;
+    }
+    else {
+        sender.title = @"Edit";
+        sender.style = UIBarButtonItemStylePlain;
+    }
+}
 
 #pragma mark - addind new tasks
 
+//TODO Rewrite from UIAlertController to UIVewController somehow.
+
 -(void)addNewTask:(UIBarButtonItem *)sender {
-    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Add new task" message:@"Inter new task and description" preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Add new task" message:@"Write new task name and description" preferredStyle:UIAlertControllerStyleAlert];
     UIAlertAction *ok = [UIAlertAction actionWithTitle:@"Add" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
         UITextField *taskNameField = [alert.textFields objectAtIndex:0];
         UITextField *taskDescriptionField = [alert.textFields objectAtIndex:1];
@@ -52,9 +68,11 @@
     [alert addTextFieldWithConfigurationHandler:^(UITextField *taskDescriptionField) {
         taskDescriptionField.placeholder = @"Description";
     }];
-    [self presentViewController:alert animated:YES completion:nil];}
- 
-#pragma mark - datasource
+    [self presentViewController:alert animated:YES completion:nil];
+}
+
+
+#pragma mark - Table datasource
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
     return 1;
@@ -90,8 +108,21 @@
     self.tasks[indexPath.row] = task;
     UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
     cell.accessoryType = ([task[@"completed"] boolValue]) ? UITableViewCellAccessoryCheckmark : UITableViewCellAccessoryNone;
-    
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
 }
 
+-(UITableViewCellEditingStyle)tableView:(UITableView *)tableView editingStyleForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return UITableViewCellEditingStyleDelete;
+}
+
+-(BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    return YES;
+}
+
+-(void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        [self.tasks removeObjectAtIndex:indexPath.row];
+        [tableView reloadData];
+    }
+}
 @end
