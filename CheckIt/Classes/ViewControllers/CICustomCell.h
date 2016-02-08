7556916520ed267cd32c2c43a81aab10333a5907
DmitryKretsu <dmitry.kretsu@gmail.com>
2016-02-08 11:55:58 +0300

    Custom cell option.

---
+++ CheckIt/Classes/ViewControllers/CICustomCell.h
@@ -0,0 +1,13 @@
+//
+//  CICustomCell.h
+//  CheckIt
+//
+//  Created by DmitryKretsu on 08.02.16.
+//  Copyright © 2016 Weezlabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CICustomCell : UITableViewCell
+
+@end
---
+++ CheckIt/Classes/ViewControllers/CICustomCell.m
@@ -0,0 +1,23 @@
+//
+//  CICustomCell.m
+//  CheckIt
+//
+//  Created by DmitryKretsu on 08.02.16.
+//  Copyright © 2016 Weezlabs. All rights reserved.
+//
+
+#import "CICustomCell.h"
+
+@implementation CICustomCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
